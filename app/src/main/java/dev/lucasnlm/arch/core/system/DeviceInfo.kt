6b6990f92b685f9f5a1ac3ffe61b8b76ad214484
Lucas Nunes <lucasnunesk@gmail.com>
2019-06-07 09:45:48 -0300

    Extract Device info to other file and fix CPU_ABI deprecation

--- app/src/main/java/dev/lucasnlm/arch/core/system/DeviceInfo.kt
+++ app/src/main/java/dev/lucasnlm/arch/core/system/DeviceInfo.kt
@@ -1,18 +1,6 @@
 package dev.lucasnlm.arch.core.system
 
-import android.os.Build
-
 interface DeviceInfo {
     fun getCpuCoresNumber(): Int
-
     fun getPlatformAbi(): String
-}
-
-open class NativeDeviceInfo: DeviceInfo {
-
-    override fun getCpuCoresNumber(): Int = readCpuCoresNumber()
-
-    override fun getPlatformAbi(): String = Build.CPU_ABI // TODO fix this deprecated line
-
-    private external fun readCpuCoresNumber(): Int
 }
