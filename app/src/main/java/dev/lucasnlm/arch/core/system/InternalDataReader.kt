70ea628ff1499304abd46fb3433aa69af2aaf7f3
Lucas Lima <lucasnunesk@gmail.com>
2019-07-09 00:14:17 -0300

    Fix open file on InternalDataReader

--- app/src/main/java/dev/lucasnlm/arch/core/system/InternalDataReader.kt
+++ app/src/main/java/dev/lucasnlm/arch/core/system/InternalDataReader.kt
@@ -3,5 +3,5 @@ package dev.lucasnlm.arch.core.system
 import java.io.File
 
 open class InternalDataReader: DataReader() {
-    override fun read(source: String): String = File(source).reader().readText()
+    override fun read(source: String): String = File(source).reader().use { it.readText() }
 }
--- app/src/test/java/dev/lucasnlm/arch/core/system/InternalDataReaderTest.kt
+++ app/src/test/java/dev/lucasnlm/arch/core/system/InternalDataReaderTest.kt
@@ -10,22 +10,27 @@ class InternalDataReaderTest {
     @Test
     fun testRead() {
         val content = "The quick brown fox jumps over the lazy dog."
-        val fileName = "test.txt"
-        val file = File(fileName).apply {
+        val file = File(TEST_FILE).apply {
             writeText(content)
         }
 
         assertTrue(file.exists())
 
         val internalDataReader = InternalDataReader()
-        val readText: String = internalDataReader.read(fileName)
+        val readText: String = internalDataReader.read(TEST_FILE)
         assertEquals(content, readText)
+
         file.delete()
+        assertFalse(file.exists())
     }
 
     @Test(expected = FileNotFoundException::class)
     fun testReadInvalidFile() {
         val internalDataReader = InternalDataReader()
-        assertNull(internalDataReader.read("test.txt"))
+        assertNull(internalDataReader.read(TEST_FILE))
+    }
+
+    companion object {
+        const val TEST_FILE = "test.txt"
     }
 }
